leolabs/json-autotranslate

commit 14d2f959ff6657a2f142c5589d96f2627043e307

added support for plurals/nested tokens for icu format (#87)
* added support for plurals/nested tokens for icu format

* Apply suggestions from code review

Co-authored-by: Leo Bernard <leo@leolabs.org>

---------

Co-authored-by: Leo Bernard <leo@leolabs.org>

src/matchers/icu.ts
@@ -1,21 +1,48 @@
 import { parse } from 'messageformat-parser';
 import { Matcher } from '.';
 
+type Plural = {
+  cases: [
+    {
+      tokens: string[];
+    },
+  ];
+};
+
+type ICUMatch = Plural | string;
+
 export const matchIcu: Matcher = (
   input: string,
   replacer: (i: number) => string,
 ) => {
+  const writeTokens = (part: ICUMatch) => {
+    if (part?.cases?.length) {
+      return part.cases
+        .map((partCase) => {
+          return partCase.tokens.length
+            ? `(.*)${nestedIcuMatcher(partCase.tokens)}(.*)`
+            : '';
+        })
+        .join('');
+    } else {
+      return '(.*)';
+    }
+  };
+  const nestedIcuMatcher = (parts: ICUMatch[]): string => {
+    return (
+      parts
+        .map((part) =>
+          typeof part === 'string'
+            ? part.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+            : writeTokens(part),
+        )
+        .join('')
+        // reduce replacement noise between replacements from nested tokens i.e. back to back (.*)(.*)
+        .replace(/(\(\.\*\)){2,}/g, '(.*)')
+    );
+  };
   const parts = parse(input);
-
-  const regex = new RegExp(
-    parts
-      .map((part: string) =>
-        typeof part === 'string'
-          ? part.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-          : '(.*)',
-      )
-      .join(''),
-  );
+  const regex = new RegExp(nestedIcuMatcher(parts));
 
   const matches = input.match(regex);
 

src/matchers/icu.spec.ts
@@ -1,5 +1,5 @@
 import { matchIcu } from './icu';
-import { replaceInterpolations } from '.';
+import { reInsertInterpolations, replaceInterpolations } from '.';
 
 describe('ICU replacer', () => {
   it('should not error when no placeholders are present', () => {
@@ -37,5 +37,38 @@ describe('ICU replacer', () => {
       { from: '{test}', to: '<span translate="no">0</span>' },
       { from: '{placeholders}', to: '<span translate="no">1</span>' },
     ]);
+  });
+
+  it('should replace plural ICU syntax with placeholders', () => {
+    const { clean, replacements } = replaceInterpolations(
+      '{count} {count, plural, =1 {one person} =2 {two people} other {many people}}',
+      matchIcu,
+    );
+    expect(clean).toEqual(
+      '<span translate="no">0</span> <span translate="no">1</span>one person<span translate="no">2</span>two people<span translate="no">3</span>many people<span translate="no">4</span>',
+    );
+    expect(replacements).toEqual([
+      {
+        from: '{count} {count, plural, =1',
+        to: '<span translate="no">0</span>',
+      },
+      {
+        from: '{',
+        to: '<span translate="no">1</span>',
+      },
+      {
+        from: '} =2 {',
+        to: '<span translate="no">2</span>',
+      },
+      {
+        from: '} other {',
+        to: '<span translate="no">3</span>',
+      },
+      {
+        from: '}}',
+        to: '<span translate="no">4</span>',
+      },
+    ]);
+    expect(reInsertInterpolations(clean, replacements)).toEqual('{count} {count, plural, =1 {one person} =2 {two people} other {many people}}')
   });
 });

commit 01f08a47dd3b99a806db59a519a7ce6b2218d7d1

:bug: Fix a type error introduced by #87

src/matchers/icu.ts
@@ -16,7 +16,7 @@ export const matchIcu: Matcher = (
   replacer: (i: number) => string,
 ) => {
   const writeTokens = (part: ICUMatch) => {
-    if (part?.cases?.length) {
+    if (typeof part !== 'string' && part?.cases?.length) {
       return part.cases
         .map((partCase) => {
           return partCase.tokens.length